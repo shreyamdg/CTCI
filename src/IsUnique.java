e027e1061e33e1e1fe49cc5226afc3bf2592e169
shreyam15 <shreyam.duttagupta@gmail.com>
2018-06-24 16:11:42 -0500

    checked few other cases

--- src/IsUnique.java
+++ src/IsUnique.java
@@ -1,20 +1,23 @@
 
 public class IsUnique {
-	
 	public static boolean isUnique(String str) {
 		str = str.toLowerCase().replaceAll("\\s+", "");
 		StringBuilder newStr = new StringBuilder();
-		for(char ch : str.toCharArray()) {
-			if(newStr.indexOf(Character.toString(ch)) == -1) {
+		for (char ch : str.toCharArray()) {
+			if (newStr.indexOf(Character.toString(ch)) == -1) {
 				newStr.append(ch);
-			}else
+			} else
 				return false;
 		}
-		
+
 		return true;
 	}
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
 		System.out.println(isUnique("shreyamM"));
+		System.out.println(isUnique("shreyam"));
+		System.out.println(isUnique("abcd efgh xyz"));
+		System.out.println(isUnique("       "));
+		System.out.println(isUnique("shreyamM      ree"));
 	}
 }

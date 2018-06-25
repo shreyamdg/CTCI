8d365ac32b21246afe2cf89b4b6fa10e0de1fce4
shreyam15 <shreyam.duttagupta@gmail.com>
2018-06-24 22:52:46 -0500

    without using StringBuilder

--- src/IsUnique.java
+++ src/IsUnique.java
@@ -12,6 +12,18 @@ public class IsUnique {
 
 		return true;
 	}
+	
+	public static boolean isUniquewithBoolean(String str) {
+		
+		boolean []checkStr = new boolean[128];
+		for(int i = 0; i< str.length(); i++) {
+			if(checkStr[str.charAt(i)]) {
+				return false;
+			}
+			checkStr[str.charAt(i)] = true;
+		}
+		return true;
+	}
 
 	public static void main(String[] args) {
 		System.out.println(isUnique("shreyamM"));
@@ -19,5 +31,14 @@ public class IsUnique {
 		System.out.println(isUnique("abcd efgh xyz"));
 		System.out.println(isUnique("       "));
 		System.out.println(isUnique("shreyamM      ree"));
+		
+		System.out.println(("--------With isUniquewithBoolean ------------"));
+		
+		System.out.println(isUniquewithBoolean("shreyamM"));
+		System.out.println(isUniquewithBoolean("shreyam"));
+		System.out.println(isUniquewithBoolean("abcd efgh xyz"));
+		System.out.println(isUniquewithBoolean("       "));
+		System.out.println(isUniquewithBoolean("shreyamM      ree"));
+		
 	}
 }

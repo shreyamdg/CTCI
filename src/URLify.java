b086a48109613c425b0ec45681f088076c9d3d52
shreyam15 <shreyam.duttagupta@gmail.com>
2018-07-01 02:17:25 -0500

    in place insertion

--- src/URLify.java
+++ src/URLify.java
@@ -15,10 +15,44 @@ public class URLify {
 		return s;
 	}
 	
+	//in place insertion
+	public static String urlifyCharArray(char [] str, int trueLength) {
+		int spaceCount = 0, index, i = 0;
+		for(i = 0; i<trueLength; i++) {
+			if(str[i] == ' ')
+				spaceCount++;
+		}
+		index = trueLength + spaceCount*2;
+		if(trueLength < str.length) 
+			str[trueLength] = '\0';
+		
+		for(i = trueLength-1; i>0; i--) {
+			if(str[i] == ' ') {
+				str[index - 1] = '0';
+				str[index - 2] = '2';
+				str[index - 3] = '%';
+				index = index-3;
+			}else {
+				str[index-1] = str[i];
+				index--;
+			}
+				
+		}
+		
+		//for(char ch:str) {
+			System.out.println(str.toString());
+		//}
+		
+		return "";
+		
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(urlifyString("Mr Jhon Smith   ", 13));
-		System.out.println(urlifyString("ab c", 4));
-		System.out.println(urlifyString(" ", 1));
+		String str = "Mr Jhon Smith    "; 
+		urlifyCharArray(str.toCharArray(), 13);
+		//System.out.println(urlifyString("Mr Jhon Smith    ", 13));
+		//System.out.println(urlifyString("ab c", 4));
+		//System.out.println(urlifyString(" ", 1));
 		
 		
 	}

679e7966e81a18a8f4ba120b3165ece133f5dbc9
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-07 22:10:20 -0500

    using recursive solution

--- src/linkedlist/AddTwoNums.java
+++ src/linkedlist/AddTwoNums.java
@@ -20,4 +20,25 @@ public class AddTwoNums {
 
 		return head.next;
 	}
+	
+	
+	//Recursive Solution: 
+	
+	public ListNode addTwoNumbersRecursive(ListNode l1, ListNode l2) {
+        return helper(l1, l2, 0);
+    }
+    
+    private ListNode helper(ListNode l1, ListNode l2, int carry){
+        if(l1==null && l2==null && carry==0)
+            return null;
+        int sum=carry;
+        if(l1!=null) sum+= l1.val;
+        if(l2!=null) sum+= l2.val;
+        
+        ListNode head = new ListNode(sum%10);
+        ListNode more= helper(l1!=null?l1.next: null, l2!=null?l2.next:null, sum/10);
+        head.next=more;
+        
+        return head;
+    }
 }

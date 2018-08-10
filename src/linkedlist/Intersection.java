2e715ebd553d5546a7cc6d36d0c945865c9ddd8e
Shreyam Duttagupta <shreyam.duttagupta@gmail.com>
2018-08-09 21:32:15 -0500

    comments to explain the loop

--- src/linkedlist/Intersection.java
+++ src/linkedlist/Intersection.java
@@ -5,6 +5,8 @@ public class Intersection {
 		ListNode currA = a;
 		ListNode currB = b;
 		
+		// if no loop is present, both linked list reaches the end of node after two iteration and currA == currB i.e null == null
+		// and breaks out of the loop. 
 		while(currA != currB) {
 			currA = currA == null? b : currA.next;
 			currB = currB == null? a : currB.next;

c0b2d456f19c2519500a07a021c213293634a689
shreyam15 <shreyam.duttagupta@gmail.com>
2018-08-22 20:38:24 -0500

    added code from ctci git repo

---
+++ src/stacksandQueues/FullStackException.java
@@ -0,0 +1,17 @@
+package stacksandQueues;
+
+/*
+ * author : Gayle Laakmann McDowell
+ */
+
+public class FullStackException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public FullStackException(){
+        super();
+    }
+
+    public FullStackException(String message){
+        super(message);
+    }
+}

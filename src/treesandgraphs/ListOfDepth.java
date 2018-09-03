ff0e9f3e0e63e7b3307cb7ae63bb8f45b24e2352
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-03 01:40:00 -0500

    using bfs

--- src/treesandgraphs/ListOfDepth.java
+++ src/treesandgraphs/ListOfDepth.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class ListOfDepth {
+	// Using pre order tree traversal:
+	
+	ArrayList<LinkedList<TreeNode>> depthListUsingPreOrder(TreeNode root) {
+		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
+		depthListUsingPreOrder(root, lists, 0);
+		return lists;
+	}
+	
 	void depthListUsingPreOrder(TreeNode root, ArrayList<LinkedList<TreeNode>> lists, int level) {
 		if(root == null) return;
 		LinkedList<TreeNode> newLevel = null;
@@ -18,9 +26,28 @@ public class ListOfDepth {
 		depthListUsingPreOrder(root.right, lists, level+1);
 	}
 	
-	ArrayList<LinkedList<TreeNode>> depthListUsingPreOrder(TreeNode root) {
-		ArrayList<LinkedList<TreeNode>> lists = new ArrayList<LinkedList<TreeNode>>();
-		depthListUsingPreOrder(root, lists, 0);
-		return lists;
+	// using breadth first search algorithm:
+	ArrayList<LinkedList<TreeNode>> depthListUsingBFS(TreeNode root){
+		ArrayList<LinkedList<TreeNode>> results = new ArrayList<LinkedList<TreeNode>>();
+		LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+		if(root != null) {
+			current.add(root);
+		}
+		
+		while(current.size() > 0) {
+			results.add(current);
+			LinkedList<TreeNode> parents = current;
+			current = new LinkedList<TreeNode>();
+			for(TreeNode node : parents) {
+				if(node.left != null) {
+					current.add(node.left);
+				}
+				if(node.right != null) {
+					current.add(node.right);
+				}
+			}
+		}
+		
+		return results;
 	}
 }

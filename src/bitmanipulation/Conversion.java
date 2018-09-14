56209ae666bbdd15ba78d7cf8c6b60e76f0cf73f
shreyam15 <shreyam.duttagupta@gmail.com>
2018-09-13 23:37:25 -0500

    second solution

--- src/bitmanipulation/Conversion.java
+++ src/bitmanipulation/Conversion.java
@@ -4,10 +4,17 @@ public class Conversion {
 
 	public static int bitSwapRequired(int a, int b) {
 		int count = 0;
-		for (int c = a ^ b; c != 0; c = c >>> 1) { 
+		for (int c = a ^ b; c != 0; c = c >>> 1) {
 			count += c & 1;
 		}
 		return count;
 	}
 
+	public static int bitSwapRequired2(int a, int b) {
+		int count = 0;
+		for (int c = a ^ b; c != 0; c = c & (c - 1)) {
+			count++;
+		}
+		return count;
+	}
 }
